d1f8d05c70c1de40813607e836f38cb246cb8768
Maria Sokolova <maria_sokolova@epam.com>
2021-12-08 16:07:51 +0300

    Updated temp

--- todo-list-temp/src/components/App.jsx
+++ todo-list-temp/src/components/App.jsx
@@ -5,16 +5,24 @@ import NewTask from "./Todos/NewTask";
 import Statistics from "./Statistics";
 
 class App extends Component {
+  state = {
+    alerts: [],
+    tasks: []
+  };
+
   render() {
     return (
       <div className="App">
         <main id="todo" role="main">
           <div className="main-content top-space-md">
             <div>
-              <NewTask />
-              <TodoList />
+              <NewTask alerts={this.state.alerts} />
+              <TodoList tasks={this.state.tasks} />
             </div>
-            <Statistics />
+            <Statistics
+              todoTasksCount={this.state.tasks.filter((x) => !x.done).length}
+              doneTasksCount={this.state.tasks.filter((x) => x.done).length}
+            />
           </div>
         </main>
       </div>
